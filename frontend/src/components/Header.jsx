80048460685939c6b3c969741f4682a30671cf6c
Tanishka Gadilkar <gadilkartanishka@gmail.com>
2026-04-03 12:43:48 +0530

    Build header component for Group Design layout

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="window-bar">
+      <img src="/logo.png" alt="Osdag logo" />
+      <h1>Group Design</h1>
+    </header>
+  );
+}
+
+export default Header;
